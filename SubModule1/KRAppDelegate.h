ff603cbab19ff1d331d037cfd0b08a79d18eeb70
Dhana Prakash Muddineti <dhana.muddineti@kroger.com>
2014-06-10 10:24:04 -0400

    Initial Commit

---
+++ SubModule1/KRAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  KRAppDelegate.h
+//  SubModule1
+//
+//  Created by Muddineti, Dhana (NonEmp) on 6/10/14.
+//  Copyright (c) 2014 KROGER. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface KRAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ SubModule1/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  SubModule1
+//
+//  Created by Muddineti, Dhana (NonEmp) on 6/10/14.
+//  Copyright (c) 2014 KROGER. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "KRAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([KRAppDelegate class]));
+    }
+}
