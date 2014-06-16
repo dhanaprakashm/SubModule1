409ad379ec1c1175fb2c16bccaaef341750e0d4a
Dhana Prakash Muddineti <dhana.muddineti@kroger.com>
2014-06-16 16:32:48 -0400

    Version 1.0

--- SubModule1/KRSubModulePresenter.m
+++ SubModule1/KRSubModulePresenter.m
@@ -14,8 +14,12 @@
 {
     UIStoryboard *story = [UIStoryboard storyboardWithName:@"SubModule1" bundle:nil];
     UIViewController *subModule1 = [story instantiateViewControllerWithIdentifier:@"SubModule1"];
+//    [mainVC presentViewController:subModule1 animated:YES completion:nil];
     
-    [mainVC presentViewController:subModule1 animated:YES completion:nil];
+    UINavigationController *navVC = [[UINavigationController alloc] initWithRootViewController:subModule1];
+    navVC.navigationItem.rightBarButtonItem = [[UIBarButtonItem alloc] initWithTitle:@"Cancel" style:UIBarButtonItemStylePlain target:self action:@selector(dismiss:)];
+    
+    [mainVC presentViewController:navVC animated:YES completion:nil];
 }
 
 - (void)pushSubModuleViewControllerFromVC:(UINavigationController *)navVC
@@ -24,6 +28,11 @@
     UIViewController *subModule1 = [story instantiateViewControllerWithIdentifier:@"SubModule1"];
     
     [navVC pushViewController:subModule1 animated:YES];
+}
+
+- (void)dismiss:(UIViewController *)viewController
+{
+    [viewController dismissViewControllerAnimated:YES completion:NO];
 }
 
 @end
